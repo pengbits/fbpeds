018b5d7d28a82609a2b7f6f20034bbfacf1ee420
pengbits <pengbits@gmail.com>
2025-03-25 11:45:12 -0400

    first test passes

--- app.test.js
+++ app.test.js
@@ -3,9 +3,15 @@ const app = require('./app')
 
 describe('patients', () => {
   describe('GET /patients', () => {
-    it('returns a list a patients', () => {
-      return request(app).get('/api/patients')
-              .expect('Content-Type', /json/)
+    it('returns a list a patients', async () => {
+      const {body} = await request(app)
+        .get('/api/patients')
+        .set('Accept', 'application/json')
+
+      expect(body.length).toBeGreaterThan(0)
+      const entry = body[0]
+      const attrs = Object.keys(entry)
+      expect(attrs).toEqual(expect.arrayContaining(['id','name','birthdate']))
     })
   })
 })
