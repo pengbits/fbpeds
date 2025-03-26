02e169cdfaa2bb1b2cb84d30d4a34b3df2249ba3
pengbits <pengbits@gmail.com>
2025-03-26 11:49:33 -0400

    DRYed up patient routes

--- models/Patient.js
+++ models/Patient.js
@@ -18,14 +18,17 @@ const related_types = [
 ];
 
 Patient.prototype.find = async (id, opts={}) => {
-  const query = opts.include && related_types.includes(opts.include) ? 
-    `SELECT * FROM patients JOIN ${opts.include} ON ${opts.include}.patient_id=patients.id WHERE patients.id = $1` :
-    `SELECT * FROM patients WHERE patients.id = $1`
-  ;
-  console.log(query)
   try {
-    const {rows} = await pool.query(query, [id]);
+    if(opts.include && !related_types.includes(opts.include)){
+      throw new Error(`'${opts.include}' is not a valid type to include in Patient#find()`)
+    }
+    const query = opts.include ? 
+      `SELECT * FROM patients JOIN ${opts.include} ON ${opts.include}.patient_id=patients.id WHERE patients.id = $1` :
+      `SELECT * FROM patients WHERE patients.id = $1`
+    ; 
 
+    console.log(query)
+    const {rows} = await pool.query(query, [id]);
 
     if(rows.length === 0){
       throw new Error('could not find any records with id provided:'+id)
--- routes/patients.js
+++ routes/patients.js
@@ -13,41 +13,27 @@ router.get('/', async (req, res) => {
   }
 });
 
-
-router.get('/:id', async (req, res) => {
+const getPatient = async (req, res, opts={}) => {
   try {
     const {id} = req.params
-    const row = await Patient.find(Number(id))
+    const row = await Patient.find(Number(id), opts)
     res.json(row)
   } catch (e){
     res.status(400).json({
       error: e.message
     })
   }
+}
+router.get('/:id',  (req, res) => {
+  return getPatient(req, res)
 })
 
-router.get('/:id/visits', async (req, res) => {
-  try {
-    const {id} = req.params
-    const row = await Patient.find(Number(id), {include:'visits'})
-    res.json(row)
-  } catch (e){
-    res.status(400).json({
-      error: e.message
-    })
-  }
+router.get('/:id/visits', (req, res) => {
+  return getPatient(req, res, {include:'visits'})
 })
 
-router.get('/:id/prescriptions', async (req, res) => {
-  try {
-    const {id} = req.params
-    const row = await Patient.find(Number(id), {include:'prescriptions'})
-    res.json(row)
-  } catch (e){
-    res.status(400).json({
-      error: e.message
-    })
-  }
+router.get('/:id/prescriptions', (req, res) => {
+  return getPatient(req, res, {include:'prescriptions'})
 })
 
 module.exports =  router;
