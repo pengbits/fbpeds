0346f9ca8339f8cc3869c9b2046b99bb22e3d8db
pengbits <pengbits@gmail.com>
2025-03-29 12:42:48 -0400

    wrangling configs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
+import { configDefaults } from 'vitest/config'
 import react from "@vitejs/plugin-react";
-
+console.log(configDefaults.exclude)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './client/testSetup.js',
+    exclude: [
+      ...configDefaults.exclude, "./server/**"
+    ]
   }
 });
