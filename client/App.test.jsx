8dc142547849366188f240e63c807e0b342f970c
pengbits <pengbits@gmail.com>
2025-03-29 12:48:46 -0400

    edits

--- client/App.test.jsx
+++ client/App.test.jsx
@@ -1,11 +1,11 @@
 import {describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react'
+import { render, screen, act } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
-  it('renders the App component', () => {
-    render(<App />)
+  it('renders the App component', async () => {
+    const rendered = await act(() => render(<App />))
     screen.debug()
-    // expect(screen.getByText('Patients')).toBeInTheDocument();
+      expect(screen.getByText('Patients')).toBeInTheDocument();
   })
 })
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import { configDefaults } from 'vitest/config'
 import react from "@vitejs/plugin-react";
-console.log(configDefaults.exclude)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
