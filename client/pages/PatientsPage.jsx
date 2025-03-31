6cd94860d28af71907dde09cf4f945af25d23769
pengbits <pengbits@gmail.com>
2025-03-31 12:30:09 -0400

    mini refactor

--- client/pages/PatientsPage.jsx
+++ client/pages/PatientsPage.jsx
@@ -1,13 +1,27 @@
-import { useLoaderData } from "react-router"
+import { useLoaderData, NavLink } from "react-router"
+
+const PatientLink = ({id,children}) => (
+  <NavLink to={`/patients/${id}`}>
+    {children}
+  </NavLink>
+)
+
 const PatientsPage = () => {
   const {patients} = useLoaderData()
+
   return (<div className="patients">
     <h2>Patients</h2>
     {patients && patients.map(({name,image,id}) => (
       <div key={id} className="patient">
-        <h3>{name}</h3>
+        <h3>
+          <PatientLink id={id}>
+          {name}
+          </PatientLink>
+        </h3>
         <div className="patient__image">
-          <img src={image} />
+          <PatientLink id={id}>
+            <img src={image} />
+          </PatientLink>
         </div>
       </div>
     ))}
