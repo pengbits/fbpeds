a5f5562e4edeafdeae8de7ebe2af728865a193eb
pengbits <pengbits@gmail.com>
2025-04-01 15:36:01 -0400

    restored order, somewjat

--- client/api/patients/index.js
+++ client/api/patients/index.js
@@ -2,7 +2,9 @@ export const getPatients = async () => {
   const url = `/api/patients`
   console.log(`getPatients() ${url}`)
   const response = await fetch(url, {method:'GET'})
-  return await response.json()
+  const json = await response.json()
+  // console.log(json)
+  return json
 }
 
 export const getPatient = async (id) => {
