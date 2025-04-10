04da2fb08931c778b25f1da86bad8441a6238fce
pengbits <pengbits@gmail.com>
2025-04-10 12:58:36 -0400

    new tests for patient w/ appointment

---
+++ server/routes/patient-with-appointment.test.js
@@ -0,0 +1,8 @@
+// TODO
+// this is a test that hits mutliple endpoints, we want to assert
+// that a newly created appointment shows up in the patient index
+describe('patient with appointment', () => {
+  it.todo('given there is a date, patient_id, provider_id and datetime')
+  it.todo('when i post the attributes to /appointments')
+  it.todo('then /patients will include the appointment in the response')
+})
