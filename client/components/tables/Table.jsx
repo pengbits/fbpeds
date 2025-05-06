0c239e83ad5bbdda3b841ad8c44a8c20515b71a8
pengbits <pengbits@gmail.com>
2025-05-06 15:28:52 -0400

    link to provider, show provider name in visits

--- client/components/tables/Table.jsx
+++ client/components/tables/Table.jsx
@@ -5,17 +5,26 @@ import { Table, Link } from "@radix-ui/themes"
 const isDate      = column => (column == 'date')
 const isImage     = column => (column == 'image')
 const isVisitDate = column => (column == 'visit_date')
+const isProvider  = column => (column == 'provider')
 
 const formattedCellContent = (column, value, row, baseUrl) => {
   if(isDate(column)) return datePretty(value)
-  // if(isBool(column)) return !!value ? '√' : null
+  
   if(isImage(column)) return (
     !!value && <a href={value} target="_blank">📷</a>
   )
+  
   if(isVisitDate(column)) {
     const {visit_id} = row
     return (<Link asChild>
       <RouterLink to={`${baseUrl}/visits/${visit_id}`}>{datePretty(value)}</RouterLink>
+    </Link>)
+  }
+
+  if(isProvider(column)){
+    const {name,id} = value
+    return (<Link asChild>
+      <RouterLink to={`/providers/${id}`}>{name}</RouterLink>
     </Link>)
   }
   return value
